1d2fd434eb3e0c81ec633f8b47724f8d09d521a8
mohammadreza eskandari <mohammad.1392.sk@gmail.com>
2022-08-04 17:03:30 +0000

    Update backend_cli.py

--- backend_cli.py
+++ backend_cli.py
@@ -3,6 +3,7 @@ from os import getenv
 
 
 BACKEND_URL = getenv('BACKEND_URL')
+BACKEND_STG_URL = getenv('STG_BACKEND_URL')
 # BACKEND_URL = 'https://google.com'
 
 print("backend client ready..")
@@ -13,7 +14,9 @@ class BackendCli:
     def send_event(data):
         try:
             result = requests.post(BACKEND_URL, json=data, headers={'Authorization': getenv('BACKEND_URL_TOKEN'), 'Content-Type': 'application/json; charset=utf-8'})
-            print(result.status_code, result.json(),flush=True)
+            result_stg = requests.post(BACKEND_STG_URL, json=data, headers={'Authorization': getenv('BACKEND_URL_TOKEN'), 'Content-Type': 'application/json; charset=utf-8'})
+            print(result.status_code, result.json(), 'prod',flush=True)
+            print(result.status_code, result_stg.json(), 'stg',flush=True)
             return True
         except requests.exceptions.RequestException as e:
             print(e, flush=True)
