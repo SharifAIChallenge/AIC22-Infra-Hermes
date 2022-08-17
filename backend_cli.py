b7ca67ca71a14ea85a85c24b3a0fde16569cdb12
MohammadHosein Bahmani <mohammad.bahmani@tapsi.cab>
2022-08-17 23:08:34 +0430

    Fix logs

--- backend_cli.py
+++ backend_cli.py
@@ -16,8 +16,8 @@ class BackendCli:
             result = requests.post(BACKEND_URL, json=data, headers={'Authorization': getenv('BACKEND_URL_TOKEN'), 'Content-Type': 'application/json; charset=utf-8'})
             result_stg = requests.post(BACKEND_STG_URL, json=data, headers={'Authorization': getenv('BACKEND_URL_TOKEN'), 'Content-Type': 'application/json; charset=utf-8'})
             # print(result.status_code, result.json(), 'prod',flush=True)
-            print("--------- stg response: ", result_stg.status_code, result_stg, 'stg', flush=True)
-            print(result.status_code, result_stg.json(), 'stg',flush=True)
+            print("--------- STG response: ", result_stg.status_code, result_stg, flush=True)
+            print("--------- PRD response: ", result.status_code, result.json(), flush=True)
             return True
         except requests.exceptions.RequestException as e:
             print(e, flush=True)
